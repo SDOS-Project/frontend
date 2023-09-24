604c6809d3149e44a60b43aeaebd13ca8956a670
aryanteng <aryanteng892@gmail.com>
2023-09-24 16:13:56 +0530

    created store

---
+++ store/store.js
@@ -0,0 +1,13 @@
+import { apiSlice } from '@/features/api/apiSlice';
+import { configureStore } from '@reduxjs/toolkit';
+import { setupListeners } from '@reduxjs/toolkit/query';
+
+export const store = configureStore({
+  reducer: {
+    [apiSlice.reducerPath]: apiSlice.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware),
+});
+
+setupListeners(store.dispatch);
