766393813b7bda231134e6f2669b5cdafe470135
aryanteng <aryanteng892@gmail.com>
2023-09-24 16:09:10 +0530

    created api slice

---
+++ features/api/apiSlice.js
@@ -0,0 +1,8 @@
+import { BASE_URL } from '@/utils/constants';
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const apiSlice = createApi({
+  reducerPath: 'api',
+  baseQuery: fetchBaseQuery({ baseUrl: BASE_URL }),
+  endpoints: (builder) => ({}),
+});
