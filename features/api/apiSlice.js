4e976dad808b779adb6c6f7de829667461528c47
aryanteng <aryanteng892@gmail.com>
2023-09-24 22:58:55 +0530

    added jwt token to api slice

--- features/api/apiSlice.js
+++ features/api/apiSlice.js
@@ -1,8 +1,16 @@
+import { auth } from '@/firebase';
 import { BASE_URL } from '@/utils/constants';
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const apiSlice = createApi({
   reducerPath: 'api',
-  baseQuery: fetchBaseQuery({ baseUrl: BASE_URL }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: BASE_URL,
+    prepareHeaders: async (headers) => {
+      const jwtToken = auth.currentUser.getIdToken();
+      headers.set('Authorization', `Bearer ${jwtToken}`);
+      return headers;
+    },
+  }),
   endpoints: (builder) => ({}),
 });
