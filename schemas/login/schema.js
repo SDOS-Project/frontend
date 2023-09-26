a83b459f6c34feb2f1dd1fe852f185a8b43aaba9
aryanteng <aryanteng892@gmail.com>
2023-09-27 01:55:14 +0530

    basic signup validation schema

--- schemas/login/schema.js
+++ schemas/login/schema.js
@@ -3,7 +3,7 @@ import * as yup from 'yup';
 export const loginValidationSchema = yup.object().shape({
   email: yup
     .string()
-    .email('Please enter a valid email address.')
-    .required('Email is required.'),
-  password: yup.string().required('Password is required.'),
+    .email('Please enter a valid email address')
+    .required('Email is required'),
+  password: yup.string().required('Password is required'),
 });
---
+++ schemas/signup/schema.js
@@ -0,0 +1,15 @@
+export const signupValidationSchema = yup.object().shape({
+  name: yup.string().required('Name is required'),
+  email: yup
+    .string()
+    .email('Please enter a valid email')
+    .required('Email is required'),
+  password: yup
+    .string()
+    .required('Password is required')
+    .min(6, 'Password has to be longer than 6 characters'),
+  confirmPassword: yup
+    .string()
+    .oneOf([yup.ref('password'), null], 'Passwords must match')
+    .required('Confirm Password is required'),
+});
