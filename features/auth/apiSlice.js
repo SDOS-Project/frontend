d98fa66d1cf1c7e5af2d2de37dc26a388456fa15
aryanteng <aryanteng892@gmail.com>
2023-09-26 19:43:36 +0530

    changed base url

---
+++ features/auth/apiSlice.js
@@ -0,0 +1,35 @@
+import { updateUser } from './authSlice';
+
+const { apiSlice } = require('../api/apiSlice');
+
+const AUTH_BASE_URL = '/auth';
+
+const authApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    login: builder.mutation({
+      query: (credentials) => ({
+        url: `${AUTH_BASE_URL}/login`,
+        method: 'POST',
+        body: credentials,
+      }),
+    }),
+    async onQueryStarted(_, { dispatch, queryFulfilled }) {
+      try {
+        const { user } = await queryFulfilled;
+        dispatch(updateUser(user));
+        dispatch(apiSlice.util.resetApiState());
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    signup: builder.mutation({
+      query: (credentials) => ({
+        url: `${AUTH_BASE_URL}/signup`,
+        method: 'POST',
+        body: credentials,
+      }),
+    }),
+  }),
+});
+
+export const { useLoginMutation, useSignupMutation } = authApiSlice;
