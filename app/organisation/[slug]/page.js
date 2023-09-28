48c712559388cb68f3c97a1a564c68ff91f00df4
aryanteng <aryanteng892@gmail.com>
2023-09-28 21:41:42 +0530

    organisation id page

--- app/organisation/[slug]/page.js
+++ app/organisation/[slug]/page.js
@@ -1,7 +1,19 @@
-import React from 'react';
+'use client';
+import { useGetOrganisationQuery } from '@/features/organisation/apiSlice';
+import { OrganisationType } from '@/types/OrganisationType';
 
-function Organisation() {
-  return <div>Organisation</div>;
+function Organisation({ params }) {
+  const { slug } = params;
+
+  const { data: organisation, isLoading } = useGetOrganisationQuery(slug);
+
+  console.log('organisation', organisation);
+  if (isLoading) return <div>Loading...</div>;
+  return (
+    <div className='width-layout-1 body-large text-center mt-20'>
+      Hi {organisation?.name} Role: {OrganisationType[organisation?.type]}
+    </div>
+  );
 }
 
 export default Organisation;
--- app/organisation/page.js
+++ app/organisation/page.js
@@ -1,5 +1,3 @@
-import React from 'react';
-
 function Organisations() {
   return <div>Organisations</div>;
 }
