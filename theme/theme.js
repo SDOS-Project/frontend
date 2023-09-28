d266262b3138cf9e2a318cb0b66350123581e863
aryanteng <aryanteng892@gmail.com>
2023-09-28 18:02:03 +0530

    added mui theme

---
+++ theme/theme.js
@@ -0,0 +1,44 @@
+import { createTheme } from '@mui/material/styles';
+import themeConstants from './themeConstants';
+
+const theme = createTheme({
+  palette: {
+    primary: themeConstants.primary,
+    secondary: themeConstants.secondary,
+    background: themeConstants.background,
+    text: {
+      primary: themeConstants.fg.main,
+      secondary: themeConstants.fg.dark,
+    },
+    error: themeConstants.error,
+  },
+  breakpoints: {
+    values: themeConstants.breakpoints,
+  },
+  typography: {
+    h1: {
+      fontFamily: themeConstants.font.heading,
+    },
+    h2: {
+      fontFamily: themeConstants.font.heading,
+    },
+    h3: {
+      fontFamily: themeConstants.font.heading,
+    },
+    h4: {
+      fontFamily: themeConstants.font.heading,
+    },
+    h5: {
+      fontFamily: themeConstants.font.heading,
+    },
+    h6: {
+      fontFamily: themeConstants.font.heading,
+    },
+    button: {
+      textTransform: 'none',
+    },
+    fontFamily: themeConstants.font.body,
+  },
+});
+
+export { theme };
---
+++ theme/themeConstants.js
@@ -0,0 +1,36 @@
+const themeConstants = {
+  primary: {
+    dark: '#08453A',
+    light: '#3C8276',
+    main: '#0C6354',
+  },
+  secondary: {
+    dark: '#2E599C',
+    light: '#6899E6',
+    main: '#4380E0',
+  },
+  error: {
+    dark: '#d32f2f',
+    light: '#e57373',
+    main: '#f44336',
+  },
+  fg: { main: '#212121', dark: '#757575' },
+  background: {
+    default: '#f3f2ef',
+    paper: '#ffffff',
+  },
+  breakpoints: {
+    xs: '0px',
+    sm: '640px',
+    md: '768px',
+    lg: '1024px',
+    xl: '1280px',
+    '2xl': '1536px',
+  },
+  font: {
+    heading: "'Fira Sans', 'sans-serif'",
+    body: "'Assistant', 'sans-serif'",
+  },
+};
+
+export default themeConstants;
