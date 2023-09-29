6d9a991e30d54d6eb41019a29efdf011b7e210c6
aryanteng <aryanteng892@gmail.com>
2023-09-30 00:59:01 +0530

    organisations page made

--- app/organisation/page.js
+++ app/organisation/page.js
@@ -1,4 +1,11 @@
+import { useGetOrganisationsQuery } from '@/features/organisation/apiSlice';
+
 function Organisations() {
+  const { data: organisations, isLoading: isOrganisationsLoading } =
+    useGetOrganisationsQuery();
+
+  console.log('organisations', organisations);
+
   return <div>Organisations</div>;
 }
 
