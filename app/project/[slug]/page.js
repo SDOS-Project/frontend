2d2ac41ebac5cb04051ca9dfff3f3f05018b368f
aryanteng <aryanteng892@gmail.com>
2023-09-30 00:15:37 +0530

    project/id page completed

--- app/project/[slug]/page.js
+++ app/project/[slug]/page.js
@@ -1,5 +1,15 @@
-function Project() {
-  return <div>Project</div>;
+'use client';
+import { useGetProjectQuery } from '@/features/project/apiSice';
+
+function Project({ params }) {
+  const { slug } = params;
+
+  const { data: project, isLoading } = useGetProjectQuery(slug);
+
+  console.log('project', project);
+
+  if (isLoading) return <div>Loading...</div>;
+  return <div className='mt-20'>Project handle: {slug}</div>;
 }
 
 export default Project;
