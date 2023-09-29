f9b8d1a0c240c7dff557340eaabe30f3c710abc9
aryanteng <aryanteng892@gmail.com>
2023-09-30 01:46:27 +0530

    changes

--- components/common/Layout.js
+++ components/common/Layout.js
@@ -2,11 +2,12 @@
 import { selectIsAuthenticated } from '@/features/auth/authSlice';
 import { useSelector } from 'react-redux';
 import Header from '../layout/header';
+import { useRouter } from 'next/navigation';
 
 export default function Layout({ children }) {
-  const auth = useSelector(selectIsAuthenticated);
+  const router = useRouter();
 
-  //   if (!auth) return <>Unauthorised</>;
+  const auth = useSelector(selectIsAuthenticated);
 
   return (
     <>
