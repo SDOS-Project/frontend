4a44c281f59947968296dfe968f571fb63729e30
aryanteng <aryanteng892@gmail.com>
2023-09-29 21:30:37 +0530

    project api slice

---
+++ features/project/apiSice.js
@@ -0,0 +1,27 @@
+import { apiSlice } from '../api/apiSlice';
+
+const PROJECT_BASE_URL = '/project';
+
+export const projectApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    getProjects: builder.query({
+      query: () => ({
+        url: `${PROJECT_BASE_URL}`,
+        method: 'GET',
+      }),
+    }),
+    getProject: builder.query({
+      query: (handle) => ({
+        url: `${PROJECT_BASE_URL}/${handle}`,
+        method: 'GET',
+      }),
+    }),
+    createProject: builder.mutation({
+      query: (project) => ({
+        url: `${PROJECT_BASE_URL}`,
+        method: 'POST',
+        body: project,
+      }),
+    }),
+  }),
+});
