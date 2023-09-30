3b9931c8d4b0749c9ccab831b0faa6826f11515e
aryanteng <aryanteng892@gmail.com>
2023-09-30 21:32:34 +0530

    outfit font added to global app layout

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,10 @@ import { Providers } from '@/features/redux/provider';
 import './globals.css';
 import ToastProvider from './toast.provider';
 import CustomThemeProvider from '@/theme/provider';
+import { Outfit } from 'next/font/google';
 import Layout from '@/components/common/Layout';
+
+const outfit = Outfit({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'SDOS',
@@ -12,7 +15,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang='en'>
-      <body suppressHydrationWarning={true}>
+      <body suppressHydrationWarning={true} className={outfit.className}>
         <Providers>
           <CustomThemeProvider>
             <ToastProvider>
--- components/common/Layout.js
+++ components/common/Layout.js
@@ -3,11 +3,36 @@ import { selectIsAuthenticated } from '@/features/auth/authSlice';
 import { useSelector } from 'react-redux';
 import Header from '../layout/header';
 import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
 
 export default function Layout({ children }) {
   const router = useRouter();
 
   const auth = useSelector(selectIsAuthenticated);
+
+  // useEffect(() => {
+  //   // Event handler for route change start
+  //   const handleRouteChangeStart = (url) => {
+  //     console.log(`Route is changing to: ${url}`);
+  //     // You can perform actions when the route change starts here
+  //   };
+
+  //   // Event handler for route change complete
+  //   const handleRouteChangeComplete = (url) => {
+  //     console.log(`Route change completed to: ${url}`);
+  //     // You can perform actions when the route change is complete here
+  //   };
+
+  //   // Subscribe to route change events
+  //   router.events.on('routeChangeStart', handleRouteChangeStart);
+  //   router.events.on('routeChangeComplete', handleRouteChangeComplete);
+
+  //   // Clean up event listeners when the component unmounts
+  //   return () => {
+  //     router.events.off('routeChangeStart', handleRouteChangeStart);
+  //     router.events.off('routeChangeComplete', handleRouteChangeComplete);
+  //   };
+  // }, [router.events]);
 
   return (
     <>
