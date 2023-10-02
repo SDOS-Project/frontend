a7921260e159ce8d7a7b52a1937ccad262ea178b
aryanteng <aryanteng892@gmail.com>
2023-10-02 13:52:39 +0530

    layout quotes fixed

--- app/layout.js
+++ app/layout.js
@@ -1,24 +1,23 @@
-import { Providers } from "@/features/redux/provider";
-import "./globals.css";
-import ToastProvider from "./toast.provider";
-import CustomThemeProvider from "@/theme/provider";
-import { Outfit } from "next/font/google";
-import Layout from "@/components/common/Layout";
+import { Providers } from '@/features/redux/provider';
+import './globals.css';
+import ToastProvider from './toast.provider';
+import CustomThemeProvider from '@/theme/provider';
+import { Outfit } from 'next/font/google';
+import Layout from '@/components/common/Layout';
 
-const outfit = Outfit({ subsets: ["latin"] });
+const outfit = Outfit({ subsets: ['latin'] });
 
 export const metadata = {
-  title: "SDOS",
-  description: "This is the SDOS project",
+  title: 'SDOS',
+  description: 'This is the SDOS project',
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang='en'>
+    <html lang="en">
       <body
         suppressHydrationWarning={true}
-        className={outfit.className + " bg-grey min-h-screen"}
-      >
+        className={outfit.className + ' bg-grey min-h-screen'}>
         <Providers>
           <CustomThemeProvider>
             <ToastProvider>
