e45cb302847e321ccca8d5d2ecd7e2789cbb7470
Avi Vashishta <avivashishta@gmail.com>
2023-10-02 17:51:29 +0530

    organisation card

---
+++ components/organisation/OrganisationCard.js
@@ -0,0 +1,62 @@
+import { Email } from '@mui/icons-material';
+import { Avatar } from '@mui/material';
+import RoomIcon from '@mui/icons-material/Room';
+import LinkIcon from '@mui/icons-material/Link';
+import SchoolIcon from '@mui/icons-material/School';
+import { useRouter } from 'next/navigation';
+import { OrganisationType } from '@/types/OrganisationType';
+
+function OrganisationCard({
+  address,
+  email,
+  handle,
+  ipPolicy,
+  logoUrl,
+  name,
+  type,
+}) {
+  const router = useRouter();
+  return (
+    <div
+      className="w-full bg-white shadow-md cursor-pointer hover:shadow-lg"
+      onClick={() => router.push(`/organisation/${handle}`)}>
+      <div className="w-full py-4 px-6 flex justify-between items-center border-b">
+        <div className="flex justify-start items-center gap-2">
+          <Avatar className="body-xsmall" src={logoUrl ?? ''}>
+            {name.split(' ')[0].slice(0, 3)}
+          </Avatar>
+          <p className="body-normal">{name}</p>
+        </div>
+        {type.toLowerCase() === OrganisationType.ACADEMIC.toLowerCase() ? (
+          <SchoolIcon className="body-xlarge" />
+        ) : (
+          <CorporateFareIcon className="body-xlarge" />
+        )}
+      </div>
+      <div className="flex flex-col gap-2 py-4 px-6">
+        <a href={`mailto:${email}`} onClick={(e) => e.stopPropagation()}>
+          <div className="flex gap-2 items-center">
+            <Email className="body-small" />
+            <p className="body-xsmall">{email}</p>
+          </div>
+        </a>
+        <div className="flex gap-2 items-center">
+          <RoomIcon className="body-small" />
+          <p className="body-xsmall">{address}</p>
+        </div>
+
+        <div
+          className="flex gap-2 items-center"
+          onClick={(e) => {
+            e.stopPropagation();
+            window.open(ipPolicy, '_blank');
+          }}>
+          <LinkIcon className="body-small" />
+          <p className="body-xsmall">{ipPolicy}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default OrganisationCard;
