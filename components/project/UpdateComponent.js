241d5db0dc4dd63d0feec8dff03a25f016192d55
aryanteng <aryanteng892@gmail.com>
2023-10-05 23:52:45 +0530

    update component changes

--- components/project/UpdateComponent.js
+++ components/project/UpdateComponent.js
@@ -5,22 +5,20 @@ import React from 'react';
 
 function UpdateComponent({ user, content, createdAt }) {
   return (
-    <div className="my-4">
-      <div className="flex justify-between items-start gap-3">
-        <Avatar sx={{ height: '1.75rem', width: '1.75rem' }} />
-        <div className="flex flex-col w-full">
-          <div className="flex justify-between gap-3 items-center mb-2">
-            <Link href={`/user/${user.handle}`}>
-              <p className="body-small text-primary-light ">
-                {user?.firstName} {user?.lastName}
-              </p>
-            </Link>
-            <p className="body-small text-primary-light">
-              {formatDate(createdAt)}
+    <div className="flex justify-between items-start gap-3">
+      <Avatar className="w-10 h-10" />
+      <div className="flex flex-col w-full">
+        <div className="flex justify-between gap-3 items-center mb-2">
+          <Link href={`/user/${user?.handle}`}>
+            <p className="body-small text-primary-light ">
+              {user?.firstName} {user?.lastName}
             </p>
-          </div>
-          <p className="body-small">{content}</p>
+          </Link>
+          <p className="body-small text-primary-light">
+            {formatDate(createdAt)}
+          </p>
         </div>
+        <p className="body-small">{content}</p>
       </div>
     </div>
   );
