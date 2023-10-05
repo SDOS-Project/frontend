02f00df6f548b1239a734f858f63b8b673aaf2e3
aryanteng <aryanteng892@gmail.com>
2023-10-05 22:49:58 +0530

    padding layout project page

--- app/project/page.js
+++ app/project/page.js
@@ -9,7 +9,7 @@ export default function Projects() {
   console.log('projects', projects);
 
   return (
-    <main className="cards-grid-layout padding-layout-1 grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-10 sm:gap-4">
+    <main className="cards-grid-layout padding-layout-2 grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-10 sm:gap-4">
       <ProjectCardsLayout
         projects={projects}
         isProjectsLoading={isProjectsLoading}
