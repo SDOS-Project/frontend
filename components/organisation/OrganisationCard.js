3e19bd8bd8400774526a893d3c55fd7592a69ff0
aryanteng <aryanteng892@gmail.com>
2023-10-05 23:39:31 +0530

    org card changes

--- components/organisation/OrganisationCard.js
+++ components/organisation/OrganisationCard.js
@@ -1,21 +1,12 @@
 import { Email } from '@mui/icons-material';
 import { Avatar, Tooltip } from '@mui/material';
 import RoomIcon from '@mui/icons-material/Room';
-import LinkIcon from '@mui/icons-material/Link';
 import SchoolIcon from '@mui/icons-material/School';
 import { OrganisationType } from '@/types/OrganisationType';
 import Link from 'next/link';
 import CorporateFareIcon from '@mui/icons-material/CorporateFare';
 
-function OrganisationCard({
-  address,
-  email,
-  handle,
-  ipPolicy,
-  logoUrl,
-  name,
-  type,
-}) {
+function OrganisationCard({ address, email, handle, logoUrl, name, type }) {
   return (
     <Link href={`/organisation/${handle}`}>
       <div className="w-full bg-white shadow-md cursor-pointer hover:shadow-lg overflow-hidden">
@@ -35,24 +26,14 @@ function OrganisationCard({
           )}
         </div>
         <div className="flex flex-col gap-2 py-4 px-6">
-          <Link href={`mailto:${email}`} onClick={(e) => e.stopPropagation()}>
-            <div className="flex gap-2 items-center">
-              <Email className="body-small" />
-              <p className="body-xsmall">{email}</p>
-            </div>
-          </Link>
+          <div className="flex gap-2 items-center">
+            <Email className="body-small" />
+            <p className="body-xsmall">{email}</p>
+          </div>
           <div className="flex gap-2 items-center">
             <RoomIcon className="body-small" />
             <p className="body-xsmall">{address}</p>
           </div>
-          <Link href={ipPolicy} target="_blank">
-            <div className="flex gap-2 items-center">
-              <LinkIcon className="body-small" />
-              <Tooltip title={ipPolicy}>
-                <p className="body-xsmall link line-clamp-1">{ipPolicy}</p>
-              </Tooltip>
-            </div>
-          </Link>
         </div>
       </div>
     </Link>
