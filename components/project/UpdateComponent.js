d843275ef83b2f200238ed4b38fca1f82c6b0054
Avi Vashishta <avivashishta@gmail.com>
2023-10-07 13:25:03 +0530

    update component done

--- components/project/UpdateComponent.js
+++ components/project/UpdateComponent.js
@@ -7,12 +7,12 @@ function UpdateComponent({ user, content, createdAt }) {
   return (
     <div className="flex justify-between items-start gap-3 mb-4">
       <Avatar className="h-8 w-8 body-xsmall">
-        {user?.firstName[0] + user?.lastName[0]}
+        {user?.firstName[0] + user?.lastName[0] ?? ''}
       </Avatar>
       <div className="flex flex-col w-full gap-1">
-        <Link href={`/user/${user.handle}`}>
-          <p className="body-normal text-primary-dark ">
-            {user?.firstName} {user?.lastName}
+        <Link href={`/user/${user?.handle}`}>
+          <p className="body-normal text-primary-dark">
+            {user?.firstName + ' ' + user?.lastName ?? ''}
           </p>
         </Link>
         <p className="body-xsmall">{content}</p>
