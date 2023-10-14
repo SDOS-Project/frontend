3a4a9fb9cc43eaa836abe54c03b39377321d33e8
aryanteng <aryanteng892@gmail.com>
2023-10-14 23:42:34 +0530

    wrapped tab styles in a use memo

--- components/signup/TabSwitch.js
+++ components/signup/TabSwitch.js
@@ -1,12 +1,19 @@
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
+import { useMemo } from 'react';
 
 export const TabSwitch = () => {
-  const selectedTab =
-    'body-xsmall p-2.5 border-2 border-primary-main bg-primary-main text-white rounded-md w-full text-center cursor-pointer duration-200 hover:bg-white hover:text-primary-main md:px-6';
+  const selectedTab = useMemo(
+    () =>
+      'body-xsmall p-2.5 border-2 border-primary-main bg-primary-main text-white rounded-md w-full text-center cursor-pointer duration-200 hover:bg-white hover:text-primary-main md:px-6',
+    []
+  );
 
-  const notSelectedTab =
-    'body-xsmall p-2.5 border-2 border-primary-main text-primary-main rounded-md w-full text-center cursor-pointer hover:bg-primary-main hover:text-white duration-200 md:px-6';
+  const notSelectedTab = useMemo(
+    () =>
+      'body-xsmall p-2.5 border-2 border-primary-main text-primary-main rounded-md w-full text-center cursor-pointer hover:bg-primary-main hover:text-white duration-200 md:px-6',
+    []
+  );
 
   const pathname = usePathname();
 
