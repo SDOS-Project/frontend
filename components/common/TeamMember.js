c45b852f14dedf3491ca69f1b1f467f6221d43fa
aryanteng <aryanteng892@gmail.com>
2023-10-16 16:49:32 +0530

    team member changes

--- components/common/TeamMember.js
+++ components/common/TeamMember.js
@@ -5,24 +5,24 @@ import Link from 'next/link';
 export default function TeamMember({ firstName, lastName, email, handle }) {
   return (
     <div className="flex justify-between items-center">
-      {/* <Link href={`/user/${handle}`}> */}
-      <div>
-        <div className="flex items-center gap-2">
-          <Avatar className="w-8 h-8 sm:h-10 sm:w-10 body-small sm:body-normal">
-            {firstName[0] + lastName[0]}
-          </Avatar>
-          <p>
-            {firstName} {lastName}
-          </p>
+      <Link href={`/user/${handle}`}>
+        <div>
+          <div className="flex items-center gap-2">
+            <Avatar className="w-8 h-8 sm:h-10 sm:w-10 body-small sm:body-normal">
+              {firstName[0] + lastName[0]}
+            </Avatar>
+            <p>
+              {firstName} {lastName}
+            </p>
+          </div>
         </div>
-      </div>
-      {/* </Link> */}
-      {/* <Link href={`mailto:${email}`} onClick={(e) => e.stopPropagation()}> */}
-      <div className="flex gap-2 items-center">
-        <Email className="body-normal" />
-        <p className="body-small">{email}</p>
-      </div>
-      {/* </Link> */}
+      </Link>
+      <Link href={`mailto:${email}`} onClick={(e) => e.stopPropagation()}>
+        <div className="flex gap-2 items-center">
+          <Email className="body-normal" />
+          <p className="body-small">{email}</p>
+        </div>
+      </Link>
     </div>
   );
 }
