e1f7eeb6e4750872dce5d6d7634321c5d79100d5
aryanteng <aryanteng892@gmail.com>
2023-10-16 16:56:30 +0530

    resolved console errors

--- app/recommended/page.js
+++ app/recommended/page.js
@@ -7,21 +7,17 @@ export default function Recommended() {
   const { data: recommeded, isLoading } = useGetRecommendedQuery();
   if (isLoading)
     return (
-      <>
-        <main className="cards-grid-layout padding-layout-2 grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-4 gap-10 sm:gap-8">
-          {Array.from({ length: 15 }).map((item) => (
-            <UserCardSkeleton key={item} />
-          ))}
-        </main>
-      </>
-    );
-  return (
-    <>
       <main className="cards-grid-layout padding-layout-2 grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-4 gap-10 sm:gap-8">
-        {recommeded?.map((user) => (
-          <UserCard key={user.handle} {...user} />
+        {Array.from({ length: 15 }).map((item, i) => (
+          <UserCardSkeleton key={`${item}-${i}`} />
         ))}
       </main>
-    </>
+    );
+  return (
+    <main className="cards-grid-layout padding-layout-2 grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-4 gap-10 sm:gap-8">
+      {recommeded?.map((user) => (
+        <UserCard key={user.handle} {...user} />
+      ))}
+    </main>
   );
 }
