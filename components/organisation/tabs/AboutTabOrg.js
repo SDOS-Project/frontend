ea455b16d12db9f75026483a321383659acba263
aryanteng <aryanteng892@gmail.com>
2023-10-16 16:53:38 +0530

    about tab org changes

--- components/organisation/tabs/AboutTabOrg.js
+++ components/organisation/tabs/AboutTabOrg.js
@@ -10,28 +10,28 @@ export default function AboutTab({ handle }) {
     <>
       <div className="w-full flex flex-col gap-2 border-b pb-4">
         <p className="body-large font-medium text-primary-dark">Contact</p>
-        {/* <Link href={`mailto:${organisation?.email}`}> */}
-        <div className="flex items-center gap-2">
-          <Email className="text-primary-main" />
-          <p className="body-small">{organisation?.email}</p>
-        </div>
-        {/* </Link> */}
+        <Link href={`mailto:${organisation?.email}`}>
+          <div className="flex items-center gap-2">
+            <Email className="text-primary-main" />
+            <p className="body-small">{organisation?.email}</p>
+          </div>
+        </Link>
       </div>
       <div className="w-full flex flex-col gap-2 border-b pb-4">
         <p className="body-large font-medium text-primary-dark">Address</p>
         <div className="flex items-center gap-2">
           <RoomIcon className="text-primary-main" />
-          <p className="body-small">{organisation.address}</p>
+          <p className="body-small">{organisation?.address}</p>
         </div>
       </div>
       <div className="w-full flex flex-col gap-2 border-b pb-4">
         <p className="body-large font-medium text-primary-dark">IP Policy</p>
-        {/* <Link href={organisation.ipPolicy} target="_blank"> */}
-        <div className="flex items-center gap-2">
-          <LinkIcon className="text-primary-main" />
-          <p className="body-small">{organisation.ipPolicy}</p>
-        </div>
-        {/* </Link> */}
+        <Link href={organisation?.ipPolicy} target="_blank">
+          <div className="flex items-center gap-2">
+            <LinkIcon className="text-primary-main" />
+            <p className="body-small">{organisation?.ipPolicy}</p>
+          </div>
+        </Link>
       </div>
     </>
   );
