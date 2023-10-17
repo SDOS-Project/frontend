01da088583dbae95994341fc62f0d973acb5ea35
aryanteng <aryanteng892@gmail.com>
2023-10-18 00:41:29 +0530

    integrated user display

--- app/user/[slug]/page.js
+++ app/user/[slug]/page.js
@@ -74,7 +74,9 @@ export default function User({ params }) {
         </Tooltip>
         <div className="w-full pt-14 sm:pt-24 z-50">
           <div className="px-2.5 sm:px-5">
-            <Avatar className="w-20 h-20 sm:w-24 sm:h-24 body-2xlarge">
+            <Avatar
+              className="w-20 h-20 sm:w-24 sm:h-24 body-2xlarge"
+              src={user?.imgUrl}>
               {user?.firstName[0] + user?.lastName[0]}
             </Avatar>
             <div className="mt-2 flex justify-between items-center">
