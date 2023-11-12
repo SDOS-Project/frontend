0e9fe5f5142447f735ce085fdba75236ca7affa7
aryanteng <aryanteng892@gmail.com>
2023-11-12 16:18:10 +0530

    key added to project cards skeleton

--- components/common/ProjectCardsLayout.js
+++ components/common/ProjectCardsLayout.js
@@ -15,8 +15,8 @@ export default function ProjectCardsLayout({
             grid ??
             'cards-grid-layout padding-layout-2 grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-10 sm:gap-8'
           }>
-          {Array.from({ length: 15 }).map((project) => (
-            <ProjectCardSkeleton key={project} />
+          {Array.from({ length: 15 }).map((project, i) => (
+            <ProjectCardSkeleton key={`${project}-${i}`} />
           ))}
         </main>
       ) : projects.length > 0 ? (
@@ -29,11 +29,12 @@ export default function ProjectCardsLayout({
             <ProjectCard key={project.handle} {...project} />
           ))}
         </main>
-      ) : (<NullViewComponent
-        imgSrc="/assets/images/project/empty.svg"
-        heading="No Projects Found"
-        desc="You haven't created any projects yet."
-      />
+      ) : (
+        <NullViewComponent
+          imgSrc="/assets/images/project/empty.svg"
+          heading="No Projects Found"
+          desc="You haven't created any projects yet."
+        />
       )}
     </>
   );
