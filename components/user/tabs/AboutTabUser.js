6cbd6ddd1c8a2b3214670871892abd025888805b
aryanteng <aryanteng892@gmail.com>
2023-11-20 00:58:19 +0530

    areas of interest condition

--- components/user/tabs/AboutTabUser.js
+++ components/user/tabs/AboutTabUser.js
@@ -1,4 +1,5 @@
 import { useGetUserQuery } from '@/features/user/apiSlice';
+import { UserRole } from '@/types/UserRole';
 import { Email } from '@mui/icons-material';
 import { Chip } from '@mui/material';
 import Link from 'next/link';
@@ -7,16 +8,18 @@ export default function AboutTabUser({ handle }) {
   const { data: user } = useGetUserQuery(handle);
   return (
     <>
-      <div className="w-full flex flex-col gap-2 py-4 px-6 border-b">
-        <p className="body-large font-medium text-primary-dark">
-          Areas Of Interest
-        </p>
-        <div className="flex flex-wrap gap-2 items-center">
-          {user?.areasOfInterest?.map((item, i) => (
-            <Chip label={item} key={`${item}-${i}`} />
-          ))}
+      {user?.role.toLowerCase() !== UserRole.STUDENT.toLowerCase() && (
+        <div className="w-full flex flex-col gap-2 py-4 px-6 border-b">
+          <p className="body-large font-medium text-primary-dark">
+            Areas Of Interest
+          </p>
+          <div className="flex flex-wrap gap-2 items-center">
+            {user?.areasOfInterest?.map((item, i) => (
+              <Chip label={item} key={`${item}-${i}`} />
+            ))}
+          </div>
         </div>
-      </div>
+      )}
       <div className="w-full flex flex-col gap-2 py-4 px-6 border-b">
         <p className="body-large font-medium text-primary-dark">Contact</p>
         <div className="flex items-center gap-2">
