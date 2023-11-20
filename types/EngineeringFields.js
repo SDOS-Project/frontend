5baf9bab912990f8fd864e34c10c68a322f7156e
aryanteng <aryanteng892@gmail.com>
2023-11-20 22:39:09 +0530

    added engineering fields object

---
+++ types/EngineeringFields.js
@@ -0,0 +1,80 @@
+export const engineeringFields = Object.freeze({
+  'Civil Engineering': [
+    'Structural Engineering',
+    'Geotechnical Engineering',
+    'Transportation Engineering',
+    'Environmental Engineering',
+  ],
+  'Mechanical Engineering': [
+    'Thermal Engineering',
+    'Manufacturing Engineering',
+    'Robotics and Automation',
+    'Materials Engineering',
+  ],
+  'Electrical Engineering': [
+    'Power Systems',
+    'Electronics and Communication Engineering',
+    'Control Systems',
+    'Power Electronics',
+  ],
+  'Computer Science and Engineering': [
+    'Artificial Intelligence (AI)',
+    'Data Science and Big Data',
+    'Software Engineering',
+    'Computer Networks',
+  ],
+  'Aerospace Engineering': [
+    'Aerodynamics',
+    'Aircraft and Spacecraft Design',
+    'Propulsion Systems',
+    'Avionics and Control Systems',
+  ],
+  'Chemical Engineering': [
+    'Chemical Process Engineering',
+    'Petrochemical Engineering',
+    'Biochemical Engineering',
+    'Environmental and Safety Engineering',
+  ],
+  'Biomedical Engineering': [
+    'Medical Imaging and Biomechanics',
+    'Biomaterials and Tissue Engineering',
+    'Medical Devices and Instrumentation',
+    'Healthcare Systems Engineering',
+  ],
+  'Civil and Environmental Engineering': [
+    'Water Resources Engineering',
+    'Geoenvironmental Engineering',
+    'Sustainable Infrastructure',
+    'Disaster Mitigation and Management',
+  ],
+  'Materials Science and Engineering': [
+    'Semiconductor Physics',
+    'Nanotechnology',
+    'Energy Systems',
+    'Computational Mechanics',
+  ],
+  'Automobile Engineering': [
+    'Vehicle Dynamics and Design',
+    'Tyre Mechanics and Design',
+    'Automotive System and Control',
+    'Intelligent Transportation System',
+  ],
+  Mechatronics: [
+    'Underwater Robotics',
+    'Medical Robotics',
+    'Aerial Robotics',
+    'Optomechatronics',
+  ],
+  'Textile Engineering': [
+    'Fibre Science & Technology',
+    'Structural Mechanics of Fibrous Assemblies',
+    'Smart Textiles',
+    'Simulation & Modeling',
+  ],
+  'Ocean Engineering': [
+    'Naval Engineering',
+    'Coastal Engineering',
+    'Offshore Engineering',
+    'Petroleum Engineering',
+  ],
+});
